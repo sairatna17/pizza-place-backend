be81b297a7ded12ed3e85a64e80163e6a197c9a6
sai <sairatna17@gmail.com>
2023-12-05 20:56:14 -0500

    first commit

---
+++ verify/mongoconnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://schap5:Ruby17@cluster0.i9mnsq7.mongodb.net/?retryWrites=true&w=majority',
+    database: 'pizzamenu',
+    collection: 'pizzacollection'
+  };
+  
